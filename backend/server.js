7c375a45dfeff203bb6aa356700a75cb31161e8e
Matt Briggs <matthew.james.briggs@gmail.com>
2020-01-05 17:15:59 -0800

    call the api server locally

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 8080;
 const app = express();
+const jsonParser = bodyParser.json()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/status", (req, res) => {
-  res.json({ status: 'ok', message: "Hello Bones 🐈" })
+  res.json({ status: 'ok', message: "Hello Bishop!" })
 });
 
 app.listen(PORT, () => {
-  console.log(`🐈 Bones prefers port ${PORT} 🐈`);
+  console.log(`🐈🐈 Bones prefers port ${PORT} 🐈🐈`);
 });
