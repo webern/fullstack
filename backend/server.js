a465cd1f48b2984c1b6f7dedb75e6f21136cf490
Matt Briggs <matthew.james.briggs@gmail.com>
2020-01-05 19:34:04 -0800

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser")
+const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 const app = express();
 const jsonParser = bodyParser.json()
+
+const allowedOrigins = ['*',
+                        '*'];
+
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     // allow requests with no origin 
+//     // (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1) {
+//       var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
